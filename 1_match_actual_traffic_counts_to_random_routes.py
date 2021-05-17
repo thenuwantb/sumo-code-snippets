47da18a13366a9e6b5afc47a42d7344d5ece5e64
Thenuwan Jayasinghe <50999345+thenuwantb@users.noreply.github.com>
2021-05-17 08:33:33 +0530

    Update 1_match_actual_traffic_counts_to_random_routes.py

--- 1_match_actual_traffic_counts_to_random_routes.py
+++ 1_match_actual_traffic_counts_to_random_routes.py
@@ -1,11 +1,15 @@
 import subprocess
 import os
 
-os.chdir('D:\\Thenuwan\\Coding\\SUMO_Python\\sumo_python\\4_Bus_priority_lane\\5_Real_Network_Cross_Junc_Dehiwala\\5_0_Wife_laptop_bus_flows')
+os.chdir('<YOUR_FOLDER_PATH')
 
 command_sumo = 'sumo -c osm.sumocfg --vehroute-output 1_vehroutes_random_trips_galle_road.xml'
 
 command_route_sampler = 'routeSampler.py -r 1_vehroutes_random_trips_galle_road.xml --edgedata-files rda_counts_2019_dehiwala.xml ' \
           ' -o 1_random_routes_match_rda_counts.rou.xml '
+
+command_xml_2_csv = 'xml2csv.py 1_random_routes_match_rda_counts_excluding_bus.rou.xml -s , -o 1_2_random_routes_match_rda_counts_excluding_bus.csv'
+
 subprocess.run(command_sumo, shell=True)
 subprocess.run(command_route_sampler, shell=True)
+subprocess.run(command_xml_2_csv, shell=True)
